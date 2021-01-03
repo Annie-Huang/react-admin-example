8010c9bb29b24751551e12d536338c1f09982eb1
Annie Huang <annie.hn@gmail.com>
2021-01-03 15:40:19 +1100

    Do Edit for record (PostEdit.js), which is very similar to PostCreate.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,19 @@ import { Admin, Resource } from 'react-admin';
 import restProvider from 'ra-data-simple-rest';
 import PostList from './components/PostList';
 import PostCreate from './components/PostCreate';
+import PostEdit from './components/PostEdit';
 
 // https://github.com/marmelab/react-admin
 function App() {
   // evn though it said 3000, the proxy setting in package.json will proxy it to 5000
   return (
     <Admin dataProvider={restProvider('http://localhost:3000')}>
-      <Resource name='posts' list={PostList} create={PostCreate} />
+      <Resource
+        name='posts'
+        list={PostList}
+        create={PostCreate}
+        edit={PostEdit}
+      />
     </Admin>
   );
 }
