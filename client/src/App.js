ea0a9cf6dfb092291ab78bab0f7b12dba372aadd
Annie Huang <annie.hn@gmail.com>
2021-01-03 15:35:41 +1100

    Create the PostCreate.js to create new record. Test it though the interface and confirmed it can create new record.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { Admin, Resource } from 'react-admin';
 import restProvider from 'ra-data-simple-rest';
 import PostList from './components/PostList';
+import PostCreate from './components/PostCreate';
 
+// https://github.com/marmelab/react-admin
 function App() {
   // evn though it said 3000, the proxy setting in package.json will proxy it to 5000
   return (
     <Admin dataProvider={restProvider('http://localhost:3000')}>
-      <Resource name='posts' list={PostList} />
+      <Resource name='posts' list={PostList} create={PostCreate} />
     </Admin>
   );
 }
